781afb19bdc952cc9a7f1e0a46ca449f76adad35
Shripad Sinari <shripad@statlab.bio5.org>
2019-06-05 11:49:58 -0700

    fixed some minor issues with run.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-tag=${2:-"1.0"}
 path=${1:-$(pwd)}
-#docker pull ssinari/smisc-base:${tag} && \
+tag=${2:-"1.0"}
+docker pull ssinari/smisc-base:${tag} && \
     docker run --rm \
 	   -v ${path}:/home/rstudio/project \
 	   -e uid=$UID \
 	   --name smisc-base_con \
-	   -it smisc-base:${tag} \
+	   -it ssinari/smisc-base:${tag} \
 	   /bin/bash
 
